941f5d24b4085bdd8c43d443e882ba9c3f3b813e
martsforever <martsforever@aliyun.com>
2019-04-12 19:36:05 +0800

    initialized project

---
+++ demo/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="app">
+        demo page
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "App"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const resolve = (dir) => path.join(__dirname, './', dir)
+
+const option = {
+    baseUrl: './',
+    lintOnSave: false,
+    outputDir: resolve('page'),
+    devServer: {
+        port: '8888',
+    },
+    pages: {
+        index: {
+            // page 的入口
+            entry: 'src/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'index.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'plain-ui demo',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'index']
+        },
+        demo: {
+            // page 的入口
+            entry: 'demo/main.js',
+            // 模板来源
+            template: 'public/index.html',
+            // 在 dist/index.html 的输出
+            filename: 'demo.html',
+            // 当使用 title 选项时，
+            // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+            title: 'plain-ui demo',
+            // 在这个页面中包含的块，默认情况下会包含
+            // 提取出来的通用 chunk 和 vendor chunk。
+            chunks: ['chunk-vendors', 'chunk-common', 'demo']
+        },
+    },
+    configureWebpack: {
+        resolve: {
+            extensions: ['.js', '.vue', '.json'],
+            alias: {
+                'src': resolve('src'),
+                'demo': resolve('demo'),
+            }
+        },
+    },
+    css: {
+        loaderOptions: {
+            sass: {
+                // data: `@import "src/styles/global.scss"; @import "demo/global.scss";`
+            }
+        }
+    },
+}
+
+module.exports = option
